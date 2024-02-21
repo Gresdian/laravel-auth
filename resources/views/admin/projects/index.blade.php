30fcec7e71390ef0401d4b3a69ca7f8b253dc379
Gresdian <chris_da98@outlook.it>
2024-02-21 23:48:21 +0100

    [UPDATE] added index and show

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between">
+                        <h2>Projects</h2>
+                        <a href="#" class="btn btn-sm btn-primary">Add project</a>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Date start</th>
+                                <th>Date end</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($project as $project)
+                                <tr>
+                                    <td>{{ $project->id }}</td>
+                                    <td>{{ $project->slug }}</td>
+                                    <td>{{ Str::limit($project->description, 20, '(...)') }}</td>
+                                    <td>{{ $project->date_start }}</td>
+                                    <td>{{ $project->date_end }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}"
+                                            class="btn btn-sm btn-square btn-primary">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
